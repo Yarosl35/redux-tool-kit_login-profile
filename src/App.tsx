e2f66650355de1e855fb10f27053a1d8c365a986
dev <dmitriy.dudolad@incode-it.com>
2021-10-05 13:24:28 +0300

    small test change

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App = () => {
                 <Link to="/registration">
                   <Menu.Item as='a'>
                     <Icon name='registered outline'/>
-                    Sig In
+                    Sign In
                   </Menu.Item>
                 </Link>
               </Sidebar>
